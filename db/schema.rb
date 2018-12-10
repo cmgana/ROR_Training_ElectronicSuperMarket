ae0b6249ce3fa01ceaf15b5ff2996b199028d171
cmgana <gana.cm@rently.com>
2018-12-10 17:34:19 +0530

    first commit esm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_093025) do
+ActiveRecord::Schema.define(version: 2018_11_14_093452) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
