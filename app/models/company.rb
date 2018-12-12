7297e51836f685f431cd2346621be24e54a5d053
cmgana <gana.cm@rently.com>
2018-12-12 18:03:16 +0530

    scope added

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,15 @@
 class Company < ApplicationRecord
-	has_many :products, dependent: :destroy
+	has_many :products, dependent: :destroy, validate: false
 	validates :contact , numericality:{ only_integer: true }
+
+	# validate do |company|
+	# 	company.products.each do |product|
+	# 		next if product.valid?
+	# 		product.errors.full_messages.each do |msg|
+	# 			errors.add_to_base(msg)
+	# 		end
+	# 	end
+	# end
+
+	scope :home_appliance_only, ->{ joins(:products).where('ptype in ("Washing machine","Television")').distinct}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :company
+  validate :product_type #,message: "Invalid product type"
+
+  private
+  def product_type
+    prod_type = ["Washing Machine","Television","Dish Washer","Rice Cooker"];
+
+    if prod_type.index(ptype) == nil
+      errors[:base] << "Invalid product type"
+      end
+  end
 end
